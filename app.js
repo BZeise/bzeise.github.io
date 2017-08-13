535a1b572c4ca23dffb28f05c999362faa6ec8de
Ben Zeise <ben.zeise@gmail.com>
2017-08-13 13:19:42 -0500

    inital commit

---
+++ app.js
@@ -0,0 +1,20 @@
+// requires
+var express = require('express');
+var path = require('path');
+var app = express();
+
+// uses
+app.use(express.static('public'));
+
+// globals
+var port = process.env.PORT || 7878;
+
+// serve index.html file, since we don't have an index.js route
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, 'public/views/index.html'));
+});
+
+// spin up server
+app.listen(port, function() {
+  console.log('up on port', port);
+});
