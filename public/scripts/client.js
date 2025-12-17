6b3dc620d24a847c719ec37344a7841b205e8f69
Ben Zeise <ben.zeise@gmail.com>
2025-12-17 16:08:55 -0600

    added slideshow, cleaned up css

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,11 +1,11 @@
 $(onReady);
 
 function onReady() {
-  console.log('jq loaded');
+  // console.log('jq loaded');
 
   // Smooth Scroll on clicking nav items
   $('nav a').click(function () {
-    console.log('clicked on it');
+    // console.log('clicked on it');
     var $href = $(this).attr('href');
     $('html,body').stop().animate({
       scrollTop: $($href).offset().top
@@ -19,6 +19,94 @@ function onReady() {
       scrollTop: 0
     }, 1000);
     return false;
+  });
+
+  // Initialize slideshow
+  initSlideshow();
+}
+
+// Slideshow functionality
+function initSlideshow() {
+  let currentSlide = 0;
+  const slides = $('.hobby-slide');
+  const indicators = $('.indicator');
+  const totalSlides = slides.length;
+
+  // Detect image orientation and apply layout class
+  slides.each(function() {
+    const $slide = $(this);
+    const $img = $slide.find('img');
+
+    // Wait for image to load to get natural dimensions
+    $img.on('load', function() {
+      const img = this;
+      const aspectRatio = img.naturalWidth / img.naturalHeight;
+
+      // If height > width (portrait), use horizontal layout
+      if (aspectRatio < 1) {
+        $slide.addClass('portrait');
+      } else {
+        $slide.removeClass('portrait');
+      }
+    });
+
+    // Trigger load event if image is already cached
+    if ($img[0].complete) {
+      $img.trigger('load');
+    }
+  });
+
+  // Show specific slide
+  function showSlide(index) {
+    // Wrap around if out of bounds
+    if (index >= totalSlides) {
+      currentSlide = 0;
+    } else if (index < 0) {
+      currentSlide = totalSlides - 1;
+    } else {
+      currentSlide = index;
+    }
+
+    // Update slides
+    slides.removeClass('active');
+    $(slides[currentSlide]).addClass('active');
+
+    // Update indicators
+    indicators.removeClass('active');
+    $(indicators[currentSlide]).addClass('active');
+  }
+
+  // Next slide
+  $('.slide-nav.next').click(function () {
+    showSlide(currentSlide + 1);
+  });
+
+  // Previous slide
+  $('.slide-nav.prev').click(function () {
+    showSlide(currentSlide - 1);
+  });
+
+  // Indicator clicks
+  indicators.click(function () {
+    const slideIndex = parseInt($(this).attr('data-slide'));
+    showSlide(slideIndex);
+  });
+
+  // Keyboard navigation
+  $(document).keydown(function (e) {
+    // Only trigger if hobbies section is in view
+    const hobbiesSection = $('#hobbies');
+    const scrollPos = $(window).scrollTop();
+    const hobbiesTop = hobbiesSection.offset().top - 200;
+    const hobbiesBottom = hobbiesTop + hobbiesSection.height();
+
+    if (scrollPos >= hobbiesTop && scrollPos <= hobbiesBottom) {
+      if (e.keyCode === 37) { // Left arrow
+        showSlide(currentSlide - 1);
+      } else if (e.keyCode === 39) { // Right arrow
+        showSlide(currentSlide + 1);
+      }
+    }
   });
 }
 
